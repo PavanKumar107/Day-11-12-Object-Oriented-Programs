29b15ec5f59557c10638950bd2e7f10acb56e0ac
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-03-25 07:48:40 +0530

    UC2

---
+++ Stock Account Management-2/src/objectorientedprograms/Account.java
@@ -0,0 +1,18 @@
+package objectorientedprograms;
+
+public class Account {
+	private static int balanceMoney;
+
+	int getBalance() {
+		return balanceMoney;
+	}
+	void depositMoney(int money) {
+		balanceMoney += money;
+		System.out.println("The current account balance is Rs." + balanceMoney);
+	}
+	void withdrawMoney(int money) {
+		balanceMoney -= money;
+	}
+	void transactionProcess() {
+	}
+}
